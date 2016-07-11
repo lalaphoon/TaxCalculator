ba9bba9739d3562c3fe4fba40698ff98b053655d
Monica <lalaphoon@gmail.com>
2016-07-10 21:03:45 -0400

    Add core data

---
+++ TaxCalculator/TaxCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxCalculator
+//
+//  Created by Mengyi LUO on 2016-07-10.
+//  Copyright © 2016 WTC Tax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
