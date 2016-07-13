44e177431465f9248d8f1751b0fe536ca4cb4890
Monica <lalaphoon@gmail.com>
2016-07-13 17:54:00 -0400

    Add 3 onboarding pages

---
+++ TaxCalculator/StepZero.swift
@@ -0,0 +1,35 @@
+//
+//  StepZero.swift
+//  TaxCalculator
+//
+//  Created by Mengyi LUO on 2016-07-13.
+//  Copyright © 2016 WTC Tax. All rights reserved.
+//
+
+import UIKit
+
+class StepZero: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor =  UIColor.customOrangeColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
