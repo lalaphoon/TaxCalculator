4f8926f4f7b0630d2704fa742a6f3a7b771f3574
Monica <lalaphoon@gmail.com>
2016-09-29 22:33:12 +0800

    Saved history view

--- TaxCalculator/SavedHistoryViewController.swift
+++ TaxCalculator/SavedHistoryViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Charts
 
-class SavedHistoryViewController: UIViewController {
-
+class SavedHistoryViewController: UIViewController, UIScrollViewDelegate {
+    var scrollView: UIScrollView!
+    var containerView: UIView!
+    @IBOutlet weak var pieChart: PieChartView!
+    @IBOutlet weak var resultChart: UITableView!
+    var showData = [[String]]()
+    let reuseIdentifier = "HistoryTableCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.scrollView = UIScrollView(frame: UIScreen.mainScreen().bounds)
+        self.scrollView.delegate = self
+        self.scrollView.contentSize = CGSizeMake(self.view.bounds.width, 667)
+        
+        self.containerView = UIView()
+        self.scrollView.userInteractionEnabled = true
+        self.containerView.userInteractionEnabled = true
+        
+        let xValues = ["Income", "Interest Income", "Others"]
+        let yValues = [20.0, 30.0, 40.0]
+        let tableData = [["Interest Income", "","", "$123.0"],
+            ["Province","","","Ontario"],
+            ["Total Income","Exclude Interest Income","","$12345"],
+            ["Federal Tax","","","$123"],
+            ["Provincial Tax","","","$12345"],
+            ["Total Tax","","","$123"]
+        ]
+        initPieChart(xValues, values:yValues)
+        initTable(tableData)
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +46,42 @@ class SavedHistoryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+         super.viewWillAppear(animated)
+         pieChart.animate(yAxisDuration: 1, easingOption: ChartEasingOption.EaseOutSine)
+    }
+    func initPieChart(dataPoints:[String], values: [Double]){
+        var dataEntries:[ChartDataEntry] = []
+        for i in 0..<dataPoints.count{
+           let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Labels for attributes")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        
+        pieChartDataSet.sliceSpace = 2.0
+        pieChartDataSet.colors = ChartColorTemplates.colorful()
+        //==============Set up for colors=======================
+        var colors: [UIColor] = []
+        colors = [UIColor.chartBlueColor(), UIColor.chartYellowColor(), UIColor.chartGreenColor(), UIColor.chartRedColor()]
+        pieChartDataSet.colors = colors
+        //======================================================
+        pieChart.descriptionText = ""
+        pieChart.descriptionTextAlign = .Center
+        pieChart.descriptionFont = UIFont(name: HEADERFONT, size: 18)
+        pieChart.setDescriptionTextPosition(x: 100.0, y: 0.0)
+        pieChart.data = pieChartData
+        
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        self.scrollView.frame = self.view.bounds
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        containerView.frame = CGRectMake(0, 0, scrollView.contentSize.width, scrollView.contentSize.height)
+    }
 
     /*
     // MARK: - Navigation
@@ -33,3 +94,31 @@ class SavedHistoryViewController: UIViewController {
     */
 
 }
+extension SavedHistoryViewController: UITableViewDataSource, UITableViewDelegate {
+    func initTable(input: [[String]]){
+        resultChart.dataSource = self
+        resultChart.delegate = self
+        let cellCollNib = UINib(nibName: "TableCell", bundle: NSBundle.mainBundle())
+        resultChart.registerNib(cellCollNib, forCellReuseIdentifier: reuseIdentifier)
+        resultChart.tableFooterView = UIView()
+        resultChart.separatorStyle = .None
+        showData = input
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return showData.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: ResultTableCell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as! ResultTableCell
+        let item = showData[indexPath.row]
+        cell.Title1.text = item[0]
+        cell.Title2.text = item[1]
+        cell.Title3.text = item[2]
+        cell.Value.text = item[3]
+        return cell
+    }
+}
+
