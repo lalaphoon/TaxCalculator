b50995e3a033ec229a1286a5509e9801d93f2399
Monica <lalaphoon@gmail.com>
2016-10-07 15:48:05 +0800

    Minor change

--- TaxCalculator/AppDelegate.swift
+++ TaxCalculator/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     func setUpFont(){
         //Setup all text font
-       // UILabel.appearance().font = UIFont(name: SMALLTITLE, size: 16)
+        UILabel.appearance().font = UIFont(name: SMALLTITLE, size: 15)
         UITextField.appearance().font =  UIFont(name: SMALLTITLE, size: 18)
         UITextView.appearance().font = UIFont(name: SMALLTITLE, size: 18)
          UIBarButtonItem.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self]).setTitleTextAttributes([NSFontAttributeName : UIFont(name: SMALLTITLE, size: 15)!], forState: .Normal)
