778319851d8146a31d8006a7be7da1e3b7bb33b5
Monica <lalaphoon@gmail.com>
2016-10-15 11:03:11 +0800

    Add Privacy Policy

---
+++ TaxCalculator/PrivacyViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PrivacyViewController.swift
+//  TaxCalculator
+//
+//  Created by Mengyi LUO on 2016-10-15.
+//  Copyright © 2016 WTC Tax. All rights reserved.
+//
+//  Reference: https://makeapppie.com/2014/10/28/swift-swift-using-uiwebviews-in-swift/
+
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        let privacyURL = NSBundle.mainBundle().URLForResource("privacy", withExtension: "html")
+        let requestObj = NSURLRequest(URL: privacyURL!)
+        myWebView.loadRequest(requestObj)
+        myWebView.backgroundColor = UIColor.whiteColor()
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
